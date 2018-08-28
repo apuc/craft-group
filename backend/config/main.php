b71bff16c161ff8e92587e1180f0dad9c8c56d38
Александр <shlykovn@mail.ru>
2018-08-28 16:08:11 +0300

    secureadminpage

--- backend/config/main.php
+++ backend/config/main.php
@@ -49,7 +49,7 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
-            'baseUrl' => '/admin',
+            'baseUrl' => '/secureadminpage',
         ],
         'user' => [
             'identityClass' => 'common\models\User',
