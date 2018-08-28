250c7866413eff135ea952c2d037cf12de7452d6
Александр <shlykovn@mail.ru>
2018-08-28 13:56:06 +0300

    acces about

--- backend/modules/about/controllers/AboutController.php
+++ backend/modules/about/controllers/AboutController.php
@@ -8,6 +8,7 @@ use backend\modules\about\controllers\AboutSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * AboutController implements the CRUD actions for About model.
