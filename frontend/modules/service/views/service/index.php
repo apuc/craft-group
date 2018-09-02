48055b86f25a57051ab9488a817f15d7dd3baa3e
Александр <shlykovn@mail.ru>
2018-09-02 23:34:17 +0300

    service

--- frontend/modules/service/views/service/index.php
+++ frontend/modules/service/views/service/index.php
@@ -32,7 +32,7 @@ $img = Url::to('@web/img/');
             <nav class="broadcrumbs">
                 <a class="broadcrumbs__link" href="<?= Url::to(['/']) ?>">Главная</a>
                 <span class="broadcrumbs__divider"> / </span>
-                <span class="broadcrumbs__curr">Услуги</span>
+                <span class="broadcrumbs__curr">Наши услуги</span>
             </nav>
 
             <div class="wrap wrap-services">
@@ -86,25 +86,25 @@ $img = Url::to('@web/img/');
                             </h3>
                             <p class="novelty__text"><?= $service[2]['description'] ?></p>
                             <a href="<?= $service[2]['href'] ?>"
-                               class="vacancies-p__vacancy-more vacancies-p__vacancy-more_novelty">подробнее</a>
+                               class="vacancies-p__vacancy-more vacancies-p__vacancy-more_novelty">Подробнее</a>
                         </div>
                     </div>
                 </div>
 
 
-                <div class="technologies">
-                    <div class="tittle">
-                        <span>Используем</span>
-                        <h2>новые технологии</h2>
-                        <p>
-                            <?= $service[1]['description'] ?>
-                        </p>
-                    </div>
-
-                    <div class="technologies__cards">
-                        <?= $service[1]['file'] ?>
-                    </div>
-                </div>
+<!--                <div class="technologies">-->
+<!--                    <div class="tittle">-->
+<!--                        <span>Используем</span>-->
+<!--                        <h2>новые технологии</h2>-->
+<!--                        <p>-->
+<!--                            --><?php//= $service[1]['description'] ?>
+<!--                        </p>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="technologies__cards">-->
+<!--                        --><?php//= $service[1]['file'] ?>
+<!--                    </div>-->
+<!--                </div>-->
 
             </div>
 
