27cf753e9ed3267d3416e1df0313fd24058d6705
Александр <shlykovn@mail.ru>
2018-09-02 23:26:20 +0300

    styles in portfolio

--- frontend/modules/portfolio/views/portfolio/more-portfolio.php
+++ frontend/modules/portfolio/views/portfolio/more-portfolio.php
@@ -25,7 +25,7 @@ $n         = 0;
 			</span>
 			<div class="grid-item__links">
 				<a class="dotportfolio" href="<?=Url::to(['single-portfolio', 'slug' => $value->slug])?>"><?=$value->title?></a>
-				<a data-pin-do="buttonPin" href="https://www.pinterest.com/pin/create/button/" data-pin-custom="true"><img src="https://addons.opera.com/media/extensions/55/19155/1.1-rev1/icons/icon_64x64.png" style="width: 25px; height: 25px;"></a>
+				<a data-pin-do="buttonPin" href="https://www.pinterest.com/pin/create/button/" data-pin-custom="true"><img src="https://addons.opera.com/media/extensions/55/19155/1.1-rev1/icons/icon_64x64.png"></a>
 			</div>
 		</div>
 	<?php endforeach;?>
