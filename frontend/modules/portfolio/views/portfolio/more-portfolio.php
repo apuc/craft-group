176bab0f43941fd3f2dd9b5d6136f7ab1cdf1de2
Александр <shlykovn@mail.ru>
2018-09-04 11:07:32 +0300

    portfolio

--- frontend/modules/portfolio/views/portfolio/more-portfolio.php
+++ frontend/modules/portfolio/views/portfolio/more-portfolio.php
@@ -14,20 +14,25 @@ use yii\helpers\Url;
 $img = Url::to('@web/img/');
 $n         = 0;
 ?>
-	<?php  foreach($more as $key => $value):?>
-		<?php $n++;?>
-		<div class="grid-item">
-			<div class="photoGrid">
-				<img class="grid-item__img" src="<?=$value->file?>">
-			</div>
-			<span class="full-size">
-				<img src="<?=$img?>full-size.svg" width="20" height="20" alt="">
-			</span>
-			<div class="grid-item__links">
-				<a class="dotportfolio" href="<?=Url::to(['single-portfolio', 'slug' => $value->slug])?>"><?=$value->title?></a>
-				<a data-pin-do="buttonPin" href="https://www.pinterest.com/pin/create/button/" data-pin-custom="true"><img src="https://addons.opera.com/media/extensions/55/19155/1.1-rev1/icons/icon_64x64.png" style="width: 25px; height: 25px; border-radius: 50%;"></a>
-			</div>
-		</div>
-	<?php endforeach;?>
+<?php  foreach($more as $key => $value):?>
+	<?php $n++;?>
+	<div class="grid-item">
+		<a class="grid-item__watch" href="<?=Url::to(['single-portfolio', 'slug' => $value->slug])?>">
+			Посмотреть работу
+		</a>
+
+		<a class="grid-item__fancybox" href="<?=$value->file?>" data-fancybox="images" data-caption="
+			<div class='portfolio__block-caption'>
+				<span><?=$value->title?></span>
+				<a href='#'>Смотреть работу на <span class='gradient'>behance.ru</span></a>
+			</div">
+
+				<span class="magnifier">
+					<img src="<?=$img?>/full-size.svg" width="20" height="20" alt="">
+				</span>
+		</a>
+		<img src="<?=$value->file?>">
+	</div>
+<?php endforeach;?>
 		<input type="hidden" id="countItems" data-count="<?=$n?>" value="<?=$n?>">
 <?php die();?>
