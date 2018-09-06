f037bb8760089b24fa30620b7e5ede479b88065f
Александр <shlykovn@mail.ru>
2018-09-07 01:42:30 +0300

    header

--- frontend/views/layouts/blog.php
+++ frontend/views/layouts/blog.php
@@ -220,7 +220,7 @@ AppAsset::register($this);
 </svg>
                     <div class="header__callback_text">
                         <span class="header__callback_top"><?= $phone->description ?? '' ?></span>
-                        <button class="header__callback_bottom">Заказать обратный звонок</button>
+	                    <a href="#phoneMassage" rel="nofollow" class="header__callback_bottom">Заказать обратный звонок</a>
                     </div>
                 </li>
             </ul>
@@ -248,7 +248,7 @@ AppAsset::register($this);
 </svg>
             <div class="header__callback_text">
                 <span class="header__callback_top"><?= $phone->description ?? '' ?></span>
-                <button class="header__callback_bottom">Заказать обратный звонок</button>
+	            <a href="#phoneMassage" rel="nofollow" class="header__callback_bottom">Заказать обратный звонок</a>
             </div>
         </div>
 	
