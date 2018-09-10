62dc0ecece925ca78540ffd9e69cce48ff80badf
Александр <shlykovn@mail.ru>
2018-09-10 11:03:43 +0300

    portfolio

--- frontend/modules/portfolio/views/portfolio/index.php
+++ frontend/modules/portfolio/views/portfolio/index.php
@@ -76,7 +76,7 @@ $img = Url::to('@web/img/');
 											<?php $w = getimagesize($home.$value['file'])[0];
 											$h = getimagesize($home.$value['file'])[1];
 											?>
-											<a href="<?=$value['file']?>" data-size="<?=$w?>x<?=$h?>"></a>
+											<a href="<?=$value['file']?>" class="portfolio-open-image" data-size="<?=$w?>x<?=$h?>"></a>
 											<a href="<?=Url::to(['single-portfolio', 'slug' => $value['slug']])?>">
 												<img src="<?=$home.'uploads/thumbnail/'.basename($value['file'])?>">
 											</a>
