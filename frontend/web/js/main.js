00ac1654847fdfd228707b0f9c263f3e7a49e64f
Александр <shlykovn@mail.ru>
2018-09-20 19:30:27 +0300

    js

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -258,7 +258,7 @@ $(document).ready(function () { // вся мaгия пoсле зaгрузки с
             processData: false,
             success: function (response) {
                 // console.log(response);
-	            if(mess === 'send_vacancy') {
+	            if(mess == 'send_vacancy') {
 		            if(!$('.brief-massage-active')) {
 			            $('#brief__mess').addClass('brief-massage-active');
 		            } else {
@@ -266,7 +266,7 @@ $(document).ready(function () { // вся мaгия пoсле зaгрузки с
 		            }
 		            $('#send_vacancy').trigger('reset');
                 }
-	            if(mess === 'send_feedback') {
+	            if(mess == 'send_feedback') {
 		            if(!$('.brief-massage-active')) {
 			            $('#feedback__mess').addClass('brief-massage-active');
 		            } else {
