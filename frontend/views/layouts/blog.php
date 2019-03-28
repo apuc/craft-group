9cc86ff5767feefe61edaca9b5ca3453ad667cc9
skat <kirill712@ukr.net>
2019-03-28 15:48:21 +0300

    добавил jivosite

--- frontend/views/layouts/blog.php
+++ frontend/views/layouts/blog.php
@@ -384,6 +384,18 @@ AppAsset::register($this);
         document.getElementById('zadarmaScripts').appendChild(script);
     }());
 </script>
+
+<!— BEGIN JIVOSITE CODE {literal} —>
+<script type='text/javascript'>
+    (function(){ var widget_id = 'D67vkHQ0XL';var d=document;var w=window;function l(){
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+        s.src = '//code.jivosite.com/script/widget/'+widget_id
+        ; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}
+        if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}
+        else{w.addEventListener('load',l,false);}}})();
+</script>
+<!— {/literal} END JIVOSITE CODE —>
+
 </body>
 </html>
 <!-- end html_close-index.html-->
