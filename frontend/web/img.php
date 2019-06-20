77f1513268e35cc0f6bef6e0355443d15a34e8e6
kavalar <apuc06@mail.ru>
2019-06-20 15:38:33 +0300

    fix img.php

--- frontend/web/img.php
+++ frontend/web/img.php
@@ -1,7 +1,8 @@
 <?php
 header('Content-type: image/jpg');
 $old_path = __DIR__ . $_SERVER['IMG_URI'];
-$width = isset($_GET['width']) ? $_GET['width'] : 1000;
+$size = getimagesize($old_path);
+$width = isset($_GET['width']) ? $_GET['width'] : $size[0];
 $pathinfo = pathinfo(substr($_SERVER['IMG_URI'], 13));
 $cache_path = __DIR__ . '/cache-img/' . $pathinfo['dirname'] . DIRECTORY_SEPARATOR . $width;
 $cache_path_with_file = __DIR__ . '/cache-img/' . $pathinfo['dirname'] . DIRECTORY_SEPARATOR . $width . DIRECTORY_SEPARATOR . $pathinfo['basename'];
